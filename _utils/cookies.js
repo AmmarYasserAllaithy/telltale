574f6e71e77f380509edbb0a021b049a89c0d093
ammar <ammaryasserallaithy@gmail.com>
2025-07-06 07:33:39 +0300

    remove helper, add cookie

---
+++ _utils/cookies.js
@@ -0,0 +1,22 @@
+
+import { TELLTALE_TOKEN_KEY } from "./api/base.js"
+
+
+const saveTokenToCookies = (token, htl = 24 * 7) =>
+    setCookie(TELLTALE_TOKEN_KEY, token, htl)
+
+
+const getTokenFromCookies = () =>
+    getCookie(TELLTALE_TOKEN_KEY)
+
+
+const clearToken = () =>
+    saveTokenToCookies('', 0)
+
+
+
+export {
+    saveTokenToCookies,
+    getTokenFromCookies,
+    clearToken,
+}
