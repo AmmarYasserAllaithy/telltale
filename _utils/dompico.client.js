b0381d6a39e7091070888510105df282e080357e
ammar <ammaryasserallaithy@gmail.com>
2025-09-18 02:54:56 +0300

    add dompico client

---
+++ _utils/dompico.client.js
@@ -0,0 +1,20 @@
+import * as dompico from 'https://ammaryasserallaithy.github.io/dompico/index.js'
+
+export const {
+  log,
+  err,
+  qs,
+  qsa,
+  appendAll,
+  create,
+  creator,
+  on,
+  onClick,
+  join,
+  rnd,
+  rndHexColor,
+  setCookie,
+  getCookie,
+  getPrototypeFunctions,
+  getObjectFunctions,
+} = dompico
